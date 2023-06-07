baa6f05e09ed23122d2a6b7aa1cad55148ba86fd
Artsiom Pashun <a.pashun@syberry.com>
2023-06-07 12:24:57 +0200

    Last fix

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,12 +1,18 @@
 package com.epam.utils;
 
+import java.util.Objects;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (org.apache.commons.lang3.StringUtils.isNumeric(str)) {
+        if (!org.apache.commons.lang3.StringUtils.isEmpty(str) && !str.equals("null")) {
             double num = Double.parseDouble(str);
             return num > 0;
         } else {
             return false;
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("null"));
     }
 }
