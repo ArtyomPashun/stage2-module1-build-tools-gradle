2385f69f089c8a6687152171d2dd7c2af43e9122
Artsiom Pashun <a.pashun@syberry.com>
2023-06-07 11:55:01 +0200

    Last fix

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,7 +3,7 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         if (org.apache.commons.lang3.StringUtils.isNumeric(str)) {
-            int num = Integer.parseInt(str);
+            double num = Double.parseDouble(str);
             return num > 0;
         } else {
             return false;
