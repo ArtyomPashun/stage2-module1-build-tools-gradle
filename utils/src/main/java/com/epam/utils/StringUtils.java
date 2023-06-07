a682df6b37791186ffd1ca584bd3f618bd021e7c
Artsiom Pashun <a.pashun@syberry.com>
2023-06-07 11:26:46 +0200

    Fixed the issue related to empty string and null string value

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return Double.parseDouble(str) > 0;
+        if (str.isEmpty() || str.equals("null")) {
+            return false;
+        } else {
+            return Double.parseDouble(str) > 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isPositiveNumber("null"));
     }
 }
