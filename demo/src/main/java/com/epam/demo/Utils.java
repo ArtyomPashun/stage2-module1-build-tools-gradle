080c54cbd53c0feb7f85da27ed6e133dd74c9418
Artsiom Pashun <a.pashun@syberry.com>
2023-06-07 11:12:06 +0200

    Fixed the issue related to floating point numbers

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         return args.stream()
-                .allMatch(str -> Integer.parseInt(str) > 0);
+                .allMatch(str -> Double.parseDouble(str) > 0);
     }
 }
